5b10bc301780295f981523af0608adfc36c96259
LuisAntonioCode <luisantoniocode@gmail.com>
2024-12-18 23:43:33 -0500

    v4.0 Ajustes adicionales en admin.py, probar API en postman y agregar archivo requirements.txt

--- school/admin.py
+++ school/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 from .models import Cursos, Horarios, Generos, Estudiantes
 
-# Register your models here.
-class EstudianteAdmin(admin.ModelAdmin): 
+@admin.register(Cursos)
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+@admin.register(Horarios)
+class HorariosAdmin(admin.ModelAdmin):
+    list_display = ('jornada',)
+
+@admin.register(Generos)
+class GenerosAdmin(admin.ModelAdmin):
+    list_display = ('genero',)
+
+@admin.register(Estudiantes)
+class EstudiantesAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos', 'genero', 'correo', 'edad', 'telefono', 'curso', 'horario', 'activo', 'fecha_registro')
     readonly_fields = ('fecha_registro',)
 
-admin.site.register(Cursos)
-admin.site.register(Horarios)
-admin.site.register(Generos)
-admin.site.register(Estudiantes, EstudianteAdmin)
+#admin.site.register(Cursos)
+#admin.site.register(Horarios)
+#admin.site.register(Generos)
+#admin.site.register(Estudiantes, EstudianteAdmin)
